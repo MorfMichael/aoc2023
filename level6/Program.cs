02b63d234518ea0becc9ed423fad306662ad8157
Michael Morf <michael.morf@codemand.at>
2023-12-06 06:58:57 +0100

    using

--- level6/Program.cs
+++ level6/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-string[] lines = File.ReadAllLines("input.txt");
+﻿string[] lines = File.ReadAllLines("input.txt");
 
 long time = long.Parse(string.Join("", lines[0].Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1)));
 long distance = long.Parse(string.Join("", lines[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1)));

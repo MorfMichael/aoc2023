32909c4d64683744eb924f293b032ffbd422bf7b
Michael Morf <michael.morf@codemand.at>
2023-12-06 06:23:48 +0100

    level6 part1

--- level6/Program.cs
+++ level6/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography.X509Certificates;
+
+string[] lines = File.ReadAllLines("input.txt");
+
+int[] time = lines[0].Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse).ToArray();
+int[] distance = lines[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse).ToArray();
+
+
+int sum = 1;
+
+for (int i = 0; i < time.Length; i++)
+{
+    int count = 0;
+    for (int t = 0; t <= time[i]; t++)
+    {
+        int dist = t * (time[i] - t);
+        if (dist > distance[i])
+            count++;
+    }
+
+    sum *= count;
+}
+
+Console.WriteLine(sum );

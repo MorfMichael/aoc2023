69d4abf93f845c5d7d4b8d24126bb659c3c385a0
Michael Morf <michael.morf@codemand.at>
2023-12-07 16:04:34 +0100

    level7 part2

--- level7/Program.cs
+++ level7/Program.cs
@@ -5,6 +5,7 @@ string[] lines = File.ReadAllLines("input.txt");
 
 Dictionary<char, int> weights = new Dictionary<char, int>
 {
+    { 'J', 1 },
     { '2', 2 },
     { '3', 3 },
     { '4', 4 },
@@ -14,25 +15,25 @@ Dictionary<char, int> weights = new Dictionary<char, int>
     { '8', 8 },
     { '9', 9 },
     { 'T', 10 },
-    { 'J', 11 },
     { 'Q', 12 },
     { 'K', 13 },
     { 'A', 14 },
 };
 
-List<(string input, Dictionary<char, int> cards, int bid, string weight)> hands = new();
+List<(string input, int bid, string weight, Dictionary<char, int> cards)> hands = new();
 
 foreach (var line in lines)
 {
     var split = line.Split();
     var cards = split[0].GroupBy(t => t).ToDictionary(t => t.Key, t => t.Count());
     var bid = int.Parse(split[1]);
-    hands.Add((split[0], cards, bid, GetWeight(split[0])));
+    hands.Add((split[0], bid, GetWeight(split[0]), cards));
 }
 
 var ordered = hands.OrderBy(t => t.weight).ToList();
-
-int sum = ordered.Select((t, i) => new { Bid = t.bid, Index = i + 1 }).Sum(x => x.Bid*x.Index);
+var bla = ordered.GroupBy(t => t.weight).Any(t => t.Count() > 1);
+Console.WriteLine(string.Join(Environment.NewLine, ordered.Where(t => true).Select(t => t.input + " - " + t.weight)));
+int sum = ordered.Select((t, i) => new { Bid = t.bid, Index = i + 1 }).Sum(x => x.Bid * x.Index);
 Console.WriteLine(sum);
 
 
@@ -49,42 +50,64 @@ string GetMapping(string input)
 
 string GetWeight(string line)
 {
-    var cards = GetCards(line);
+    var all_cards = GetCards(line);
+    var joker = all_cards.ContainsKey('J') ? all_cards['J'] : 0;
+    var cards = all_cards.Where(t => t.Key != 'J').ToDictionary();
 
-    if (cards.Any(t => t.Value == 5))
+    if (cards.Any(t => t.Value + joker == 5) || joker == 5)
     {
-        // five of a kind
-        return 7+GetMapping(line);
+        return 7 + GetMapping(line);
     }
-    else if (cards.Any(t => t.Value == 4))
+    else if (cards.Any(t => t.Value + joker == 4))
     {
-        // four of a kind
-        return 6+GetMapping(line);
+        return 6 + GetMapping(line);
     }
-    else if (cards.Any(t => t.Value == 2) && cards.Any(t => t.Value == 3))
+    else if (CheckFullHouse(cards, joker))
     {
-        // full house
-        return 5+GetMapping(line);
+        return 5 + GetMapping(line);
     }
-    else if (cards.Any(t => t.Value == 3))
+    else if (cards.Any(t => t.Value + joker == 3))
     {
-        // three of a kind
-        return 4+GetMapping(line);
+        return 4 + GetMapping(line);
     }
-    else if (cards.Count(t => t.Value == 2) >= 2)
+    else if (CheckTwoParis(cards, joker))
     {
-        // two pairs
-        return 3+GetMapping(line);
+        return 3 + GetMapping(line);
 
     }
-    else if (cards.Any(t => t.Value == 2))
+    else if (cards.Any(t => t.Value + joker == 2))
     {
-        // one pair
-        return 2+GetMapping(line);
+        return 2 + GetMapping(line);
     }
     else
     {
-        //highcard
-        return 1+GetMapping(line);
+        return 1 + GetMapping(line);
     }
+}
+
+bool CheckFullHouse(Dictionary<char, int> cards, int joker)
+{
+    var two = cards.FirstOrDefault(t => t.Value + joker == 2).Key;
+    var three = cards.FirstOrDefault(t => t.Key != two && t.Value == 3).Key;
+    if (two != default && three != default)
+        return true;
+
+    two = cards.FirstOrDefault(t => t.Value == 2).Key;
+    three = cards.FirstOrDefault(t => t.Key != two && t.Value + joker == 3).Key;
+    if (two != default && three != default)
+        return true;
+
+    return false;
+}
+
+bool CheckTwoParis(Dictionary<char, int> cards, int joker)
+{
+    var two = cards.FirstOrDefault(t => t.Value == 2).Key;
+    var other = cards.FirstOrDefault(t => t.Key != two && t.Value == 2).Key;
+    if (two != default && other != default)
+        return true;
+
+    two = cards.FirstOrDefault(t => t.Value + joker == 2).Key;
+    other = cards.FirstOrDefault(t => t.Key != two &&  t.Value == 2).Key;
+    return two != default && other != default;
 }

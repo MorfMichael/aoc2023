37e31065290f990f4f4cc12f9ef836a8185a4ace
Michael Morf <michael.morf@codemand.at>
2023-12-08 13:27:39 +0100

    level7 part2

--- level8/Program.cs
+++ level8/Program.cs
@@ -4,22 +4,45 @@ string[] lines = File.ReadAllLines("input.txt");
 
 string instructions = lines[0];
 
-Dictionary<string, (string left, string right)> data = lines.Skip(2).Select(t => t.Split(" = ")).Select(t => (pos: t[0], left: t[1].Split(",")[0][1..], right: t[1].Split(", ")[1][..^1])).ToDictionary(t => t.pos, t => (t.left,t.right));
+Dictionary<string, (string left, string right)> data = lines.Skip(2).Select(t => t.Split(" = ")).Select(t => (pos: t[0], left: t[1].Split(",")[0][1..], right: t[1].Split(", ")[1][..^1])).ToDictionary(t => t.pos, t => (t.left, t.right));
 
 int count = 0;
-string curr = "AAA";
-bool found = false;
-
+string[] current = data.Where(t => t.Key.EndsWith("A")).Select(t => t.Key).ToArray();
+Dictionary<int,int> found = new();
 while (true)
 {
     for (int i = 0; i < instructions.Length; i++)
     {
         count++;
-        curr = instructions[i] == 'R' ? data[curr].right : data[curr].left;
-        if (curr == "ZZZ")
+        for (int j = 0; j < current.Length; j++)
         {
-            Console.WriteLine(count);
-            return;
+            current[j] = instructions[i] == 'R' ? data[current[j]].right : data[current[j]].left;
+            if (current[j].EndsWith("Z") && !found.ContainsKey(j))
+            {
+                Console.WriteLine(j + ": " + count);
+                found.Add(j, count);
+            }
         }
+
+        if (found.Count == current.Length) goto result;
     }
+}
+
+result:
+Console.WriteLine(found.Aggregate(1l, (a, b) => lcm(a, b.Value)));
+
+static long gcf(long a, long b)
+{
+    while (b != 0)
+    {
+        long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+static long lcm(long a, long b)
+{
+    return (a / gcf(a, b)) * b;
 }

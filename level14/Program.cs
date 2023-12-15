9ef92ee1e4f9b40b7933be009266471ce3942736
Michael Morf <michael.morf@codemand.at>
2023-12-15 06:11:10 +0100

    level15 part1

--- level14/Program.cs
+++ level14/Program.cs
@@ -1,74 +1,158 @@
-﻿using System.Data;
+﻿using System.Collections.Concurrent;
+using System.Data;
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 string[] lines = File.ReadAllLines("input.txt");
 
 char[][] map = new char[lines.Length][];
+List<Rock> rocks = new List<Rock>();
 
 for (int y = 0; y < lines.Length; y++)
 {
-    map[y] = lines[y].ToCharArray();
-}
-
-
-Print(map);
-
-int count = 0;
-var rocks = GetRocks(map).Where(t => CanMove(t.X,t.Y,map));
-while (rocks.Any())
-{
-    Console.WriteLine(count++);
-    foreach (var rock in rocks)
+    map[y] = new char[lines[y].Length];
+    for (int x = 0; x < map[y].Length; x++)
     {
-        Move(rock.X, rock.Y, map);
-    }
+        map[y][x] = lines[y][x];
 
-    //Print(map);
-    //Console.ReadKey();
-    rocks = rocks.Where(t => CanMove(t.X, t.Y, map));
-}
-
-Console.WriteLine(GetRocks(map).Sum(x => GetLoad(x.Y, map)));
-
-void Move(int x, int y, char[][] map)
-{
-    char upper = map[y - 1][x];
-    map[y - 1][x] = map[y][x];
-    map[y][x] = upper;
-}
-
-int GetLoad(int y, char[][] map)
-{
-    return map.Length - y;
-}
-
-bool CanMove(int x, int y, char[][] map)
-{
-    return y > 0 && map[y - 1][x] == '.';
-}
-
-IEnumerable<(int X, int Y)> GetRocks(char[][] map)
-{
-    for (int y = 0; y < map.Length; y++)
-    {
-        for (int x = 0; x < map[y].Length; x++)
+        if (lines[y][x] == 'O')
         {
-            if (map[y][x] == 'O')
-                yield return (x, y);
+            map[y][x] = '.';
+            rocks.Add(new Rock(x, y));
         }
     }
 }
 
-void Print(char[][] map)
+ConcurrentDictionary<int, List<Rock>> cache = new();
+ConcurrentDictionary<string, int> cache2 = new();
+
+//Print(map, rocks);
+
+for (int i = 0; i <= 1_000_000_000; i++)
+{
+    if (i % 1_000 == 0) Console.WriteLine(i);
+
+    int hash = rocks.GetSequenceHash();
+
+    if (cache.ContainsKey(hash))
+    {
+        rocks = cache[hash];
+    }
+    else
+    {
+        rocks.OrderBy(r => r.Y).ThenBy(r => r.X).ToList().ForEach(x => x.Move(0, map, rocks));
+        rocks.OrderBy(t => t.X).ThenBy(r => r.Y).ToList().ForEach(x => x.Move(1, map, rocks));
+        rocks.OrderByDescending(t => t.Y).ThenBy(r => r.X).ToList().ForEach(x => x.Move(2, map, rocks));
+        rocks.OrderByDescending(t => t.X).ThenBy(r => r.Y).ToList().ForEach(x => x.Move(3, map, rocks));
+        cache.AddOrUpdate(hash, rocks, (k, v) => rocks);
+    }
+}
+
+Console.WriteLine(rocks.Sum(x => x.GetLoad(map.Length)));
+
+void Print(char[][] map, List<Rock> rocks)
 {
     Console.Clear();
     for (int y = 0; y < map.Length; y++)
     {
         for (int x = 0; x < map[y].Length; x++)
         {
-            Console.Write(map[y][x]);
+            if (rocks.Any(t => t.X == x && t.Y == y))
+                Console.Write('O');
+            else
+                Console.Write(map[y][x]);
         }
         Console.WriteLine();
+    }
+
+    Console.ReadKey();
+}
+
+public class Rock
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Rock(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public int GetLoad(int size)
+    {
+        return size - Y;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is Rock rock)
+        {
+            return X == rock.X && Y == rock.Y;
+        }
+
+        return base.Equals(obj);
+    }
+
+    public override string ToString()
+    {
+        return (X, Y).ToString();
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked // Overflow is fine, just wrap
+        {
+            int hash = 17;
+            // Suitable nullity checks etc, of course :)
+            hash = hash * 23 + X.GetHashCode();
+            hash = hash * 23 + Y.GetHashCode();
+            return hash;
+        }
+    }
+
+    public void Move(int direction, char[][] map, List<Rock> rocks)
+    {
+        if (direction == 0) // North
+        {
+            if (Y > 0)
+            {
+
+                while (Y - 1 >= 0 && map[Y - 1][X] == '.' && !rocks.Any(r => r.X == X && r.Y == Y - 1)) Y--;
+            }
+        }
+        else if (direction == 1) // West
+        {
+            if (X > 0)
+            {
+                while (X - 1 >= 0 && map[Y][X - 1] == '.' && !rocks.Any(r => r.Y == Y && r.X == X - 1)) X--;
+            }
+        }
+        else if (direction == 2)  // South
+        {
+            if (Y < map.Length)
+            {
+                while (Y + 1 < map.Length && map[Y + 1][X] == '.' && !rocks.Any(r => r.X == X && r.Y == Y + 1)) Y++;
+            }
+        }
+        else if (direction == 3) // South
+        {
+            if (X < map[0].Length)
+            {
+                while (X + 1 < map[0].Length && map[Y][X + 1] == '.' && !rocks.Any(r => r.Y == Y && r.X == X + 1)) X++;
+            }
+        }
+    }
+}
+
+public static class Extensions
+{
+    public static int GetSequenceHash(this IEnumerable<Rock> rocks)
+    {
+        unchecked
+        {
+            return rocks.Sum(x => x.GetHashCode());
+        }
     }
 }
---
+++ level15/Program.cs
@@ -0,0 +1,25 @@
+﻿string input = File.ReadAllText("input.txt");
+var split = input.Split(",");
+
+long sum = 0;
+foreach (var s in split)
+{
+    sum += GetHash(s);
+}
+
+Console.Write(sum);
+
+
+long GetHash(string input)
+{
+    long result = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        Console.WriteLine((int)input[i]);
+        result += (int)input[i];
+        result *= 17;
+        result %= 256;
+    }
+
+    return result;
+}

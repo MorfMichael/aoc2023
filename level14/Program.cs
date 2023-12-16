32248f5f870763474418ddb3fa6a83c11186efaa
Michael Morf <michael.morf@codemand.at>
2023-12-16 20:23:35 +0100

    level14 part2 level16 init

--- level14/Program.cs
+++ level14/Program.cs
@@ -1,21 +1,42 @@
 ﻿using System.Data;
 
-string[] lines = File.ReadAllLines("input.txt");
+string[] grid = File.ReadAllLines("input.txt");
 
-//Print(lines);
-string[] columns = GetColumns(lines);
-//Print(columns);
-columns = columns.Select(t => string.Join("#", t.Split("#").Select(x => new string(x.OrderDescending().ToArray())))).ToArray();
-//Print(columns);
+string key = string.Join(",", grid);
+HashSet<string> seen = new() { key } ;
+List<string> check = new() { key };
 
-long sum = 0;
-foreach (var c in columns)
+int i = 0;
+while (true)
 {
-    sum += c.Select((t, i) => t == 'O' ? c.Length - i : 0).Sum();
+    i++;
+    grid = Cycle(grid);
+    key = string.Join(",", grid);
+    if (seen.Contains(key))
+        break;
+    seen.Add(key);
+    check.Add(key);
 }
 
+int first = check.IndexOf(string.Join(",", grid));
+
+int index = (1_000_000_000 - first) % (i - first) + first;
+grid = check[index].Split(",");
+
+long sum = grid.Select((x, i) => (x.Split("O").Length - 1) * (grid.Length - i)).Sum();
 Console.WriteLine(sum);
 
+string[] Cycle(string[] lines)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        lines = GetColumns(lines);
+        lines = lines.Select(t => string.Join("#", t.Split("#").Select(x => new string(x.OrderDescending().ToArray())))).ToArray();
+        lines = lines.Select(t => new string(t.Reverse().ToArray())).ToArray();
+    }
+
+    return lines;
+}
 
 
 void Print(string[] columns)
@@ -25,6 +46,8 @@ void Print(string[] columns)
         Console.WriteLine(c);
     }
     Console.WriteLine();
+    Console.ReadKey();
 }
 
 string[] GetColumns(string[] lines) => Enumerable.Range(0, lines[0].Length).Select(t => new string(lines.Select(l => l[t]).ToArray())).ToArray();
+string[] GetLines(string[] columns) => Enumerable.Range(0, columns.Length).Select(t => new string(columns.Select(c => c[t]).ToArray())).ToArray();
---
+++ level16/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] lines = File.ReadAllLines("sample.txt");
+
+char[][] map = lines.Select(t => t.ToCharArray()).ToArray();
+
+HashSet<(int X, int Y)> seen = new();
+Queue<(int X, int Y, Direction Direction)> queue = new();
+
+queue.Enqueue((0, 0, Direction.Right));
+
+while (queue.Count > 0)
+{
+
+}
+
+List<(int X, int Y, Direction)>
+
+
+
+enum Direction
+{
+    Up,
+    Right,
+    Down,
+    Left,
+}

23efe07bad39cc1f6caef664c1c10bccf1a80dce
Michael Morf <michael.morf@codemand.at>
2023-12-19 21:52:02 +0100

    level17 part2

--- level17/Program.cs
+++ level17/Program.cs
@@ -1,121 +1,75 @@
-﻿string[] lines = File.ReadAllLines("sample.txt");
+﻿string[] lines = File.ReadAllLines("input.txt");
 int[][] map = lines.Select(t => t.Select(x => int.Parse(x.ToString())).ToArray()).ToArray();
 
 var end = (map[0].Length - 1, map.Length - 1);
 
-PriorityQueue<List<(int X, int Y, Direction Direction, int Cost)>, int> queue = new();
 
-queue.Enqueue([(0, 0, Direction.Right, 0)], 0);
-queue.Enqueue([(0, 0, Direction.Down, 0)], 0);
+HashSet<(int X, int Y, Direction Direction, int DCount)> seen = new();
+PriorityQueue<(int X, int Y, Direction Direction, int DirectionCount), int> queue = new();
 
-int min = int.MaxValue;
-while (queue.TryDequeue(out var current, out var cost))
+queue.Enqueue((0, 0, Direction.Right, 0), 0);
+queue.Enqueue((0, 0, Direction.Down, 0), 0);
+
+while (queue.TryDequeue(out var c, out var cost))
 {
-    if (current.Sum(x => x.Cost) > min)
+    if (seen.Contains(c))
         continue;
-    //Print(current);
-    var c = current[^1];
 
-    if ((c.X,c.Y) == end)
+    seen.Add(c);
+
+    if ((c.X,c.Y) == end && c.DirectionCount >= 4)
     {
-        int sum = current.Sum(x => x.Cost);
-        if (sum < min)
-        {
-            min = sum;
-            Console.WriteLine(sum);
-        }
-        //Console.ReadKey();
+        Console.WriteLine(cost);
+        break;
     }
 
-    var next = GetNext(current);
+    var next = GetNext(c.X, c.Y, c.Direction, c.DirectionCount);
 
     foreach (var n in next)
     {
-        int n_cost = map[n.Y][n.X];
-        int n_distance = GetDistance((n.X,n.Y), end);
-        queue.Enqueue(current.Append((n.X, n.Y, n.Direction, n_cost)).ToList(), cost + n_distance + n_cost);
+        queue.Enqueue(n, cost + map[n.Y][n.X]);
     }
 }
 
-
-int GetDistance((int X, int Y) first, (int X, int Y) second)
+List<(int X, int Y, Direction Direction, int DirectionCount)> GetNext(int x, int y, Direction direction, int dcount)
 {
-    return Math.Abs(second.X - first.X) + Math.Abs(second.Y - first.Y);
-}
+    List<(int X, int Y, Direction Direction, int DirectionCount)> result = new();
 
-List<(int X, int Y, Direction Direction)> GetNext(List<(int X, int Y, Direction Direction, int Cost)> previous)
-{
-    var last = previous[^1];
-    var last_directions = previous.TakeLast(3).Select(t => t.Direction).ToList();
-
-    List<(int X, int Y, Direction Direction)> result = new();
-
-    if (last.Direction == Direction.Up)
+    if (direction == Direction.Up)
     {
         result.AddRange([
-            (last.X, last.Y - 1, Direction.Up),
-            (last.X - 1, last.Y, Direction.Left),
-            (last.X + 1, last.Y, Direction.Right),
+            (x, y - 1, Direction.Up, dcount+1),
+            (x - 1, y, Direction.Left, 1),
+            (x + 1, y, Direction.Right, 1),
         ]);
     }
-    else if (last.Direction == Direction.Right)
+    else if (direction == Direction.Right)
     {
         result.AddRange([
-            (last.X + 1, last.Y, Direction.Right),
-            (last.X, last.Y - 1, Direction.Up),
-            (last.X, last.Y + 1, Direction.Down),
+            (x + 1, y, Direction.Right, dcount+1),
+            (x, y - 1, Direction.Up, 1),
+            (x, y + 1, Direction.Down, 1),
         ]);
     }
-    else if (last.Direction == Direction.Down)
+    else if (direction == Direction.Down)
     {
         result.AddRange([
-            (last.X, last.Y + 1, Direction.Down),
-            (last.X - 1, last.Y, Direction.Left),
-            (last.X + 1, last.Y, Direction.Right),
+            (x, y + 1, Direction.Down, dcount+1),
+            (x - 1, y, Direction.Left, 1),
+            (x + 1, y, Direction.Right, 1),
         ]);
     }
-    else if (last.Direction == Direction.Left)
+    else if (direction == Direction.Left)
     {
         result.AddRange([
-            (last.X - 1, last.Y, Direction.Left),
-            (last.X, last.Y - 1, Direction.Up),
-            (last.X, last.Y + 1, Direction.Down),
+            (x - 1, y, Direction.Left, dcount+1),
+            (x, y - 1, Direction.Up, 1),
+            (x, y + 1, Direction.Down, 1),
         ]);
     }
 
-    var resultQry = result.Where(t => t.X >= 0 && t.X < map[0].Length && t.Y >= 0 && t.Y < map.Length && !previous.Any(p => p.X == t.X && p.Y == t.Y));
-
-    if (last_directions.Count == 3 && last_directions.Distinct().Count() == 1)
-        resultQry = resultQry.Where(t => t.Direction != last_directions.First());
+    var resultQry = result.Where(t => t.X >= 0 && t.X < map[0].Length && t.Y >= 0 && t.Y < map.Length && t.DirectionCount <= 10 && (dcount >= 4 ? true : t.Direction == direction));
     return resultQry.ToList();
-}
-
-void Print(List<(int X, int Y, Direction Direction, int Cost)> path)
-{
-    Console.Clear();
-    for (int y = 0; y < map.Length; y++)
-    {
-        for (int x = 0; x < map[y].Length; x++)
-        {
-            var exists = path.FirstOrDefault(t => t.X == x && t.Y == y);
-            if (exists != default)
-            {
-                Console.Write(exists.Direction switch
-                {
-                    Direction.Up => '^',
-                    Direction.Right => '>',
-                    Direction.Left => '<',
-                    Direction.Down => 'v',
-                });
-            }
-            else
-            {
-                Console.Write(map[y][x]);
-            }
-        }
-        Console.WriteLine();
-    }
-    //Console.ReadKey();
 }
 
 enum Direction

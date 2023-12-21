8d3fd02cfdf9d7c5387466eada73204959ba84ed
Michael Morf <michael.morf@codemand.at>
2023-12-21 06:56:53 +0100

    level 20 init

---
+++ level18/Program.cs
@@ -0,0 +1,80 @@
+﻿string[] lines = File.ReadAllLines("sample.txt");
+
+Point current = (0, 0);
+List<Point> edges = new() { current };
+int sum = 0;
+foreach (var line in lines)
+{
+    var split = line.Split();
+    int steps = int.Parse(split[1]);
+    sum += steps;
+    var move = split[0] switch
+    {
+        "R" => (steps, 0),
+        "D" => (0, steps),
+        "L" => (-steps, 0),
+        "U" => (0, -steps),
+        _ => (0,0)
+    };
+
+    current += move;
+    if (current != (0, 0))
+    {
+        edges.Add(current);
+        Console.WriteLine(current);
+    }
+}
+
+var area = Math.Abs(edges.Take(edges.Count - 1)
+   .Select((p, i) => (edges[i + 1].X - p.X) * (edges[i + 1].Y + p.Y))
+   .Sum() / 2);
+
+Console.WriteLine(area);
+Console.WriteLine(sum);
+
+
+public class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public static implicit operator (int X, int Y)(Point p)
+    {
+        return (p.X, p.Y);
+    }
+
+    public static implicit operator Point((int x, int y) value)
+    {
+        return new Point
+        {
+            X = value.x,
+            Y = value.y
+        };
+    }
+
+
+    public override string ToString()
+    {
+        return (X, Y).ToString();
+    }
+
+    public static Point operator +(Point left, Point right)
+    {
+        return (left.X + right.X, left.Y + right.Y);
+    }
+
+    public static Point operator -(Point left, Point right)
+    {
+        return (left.X - right.X, left.Y - right.Y);
+    }
+
+    public static bool operator ==(Point left, Point right)
+    {
+        return left.X == right.X && left.Y == right.Y;
+    }
+
+    public static bool operator !=(Point left, Point right)
+    {
+        return !(left == right);
+    }
+}
---
+++ level20/Program.cs
@@ -0,0 +1,136 @@
+﻿
+using System.Collections.Concurrent;
+
+string[] lines = File.ReadAllLines("sample2.txt");
+
+Dictionary<string, string[]> destinations = new();
+
+foreach (var line in lines)
+{
+    var split = line.Split(" -> ");
+    string key = "%&".Contains(split[0][0]) ? split[0][1..] : split[0];
+    var D = split[1].Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+    if (destinations.ContainsKey(key))
+    {
+        destinations[key] = new List<string>([.. destinations[key], .. D]).Distinct().ToArray();
+    }
+    else
+    {
+        destinations.Add(key, D);
+    }
+}
+
+var distinct = destinations.SelectMany(t => t.Value).Distinct().Where(t => !destinations.ContainsKey(t)).ToList();
+foreach (var d in distinct)
+{
+    destinations.Add(d, []);
+}
+
+
+BroadcasterModule broadcaster = new BroadcasterModule("broadcaster", destinations);
+int low = 0, high = 0;
+//for (int i = 0; i < 1000; i++)
+{
+    low++; // button
+    var (clow, chigh) = broadcaster.HandleImpulse(false);
+    low += clow;
+    high += chigh;
+}
+Console.WriteLine(low * high);
+
+abstract class Module(string name, Dictionary<string, string[]> destinations)
+{
+    public string Name { get; set; } = name;
+
+    public List<Module> Destinations { get; set; } = destinations[name].Select(t => GetModule(t, destinations)).ToList();
+
+    public (int low, int high) HandleImpulse(bool impulse)
+    {
+        Queue<(bool impulse, Module module)> queue = new();
+        int low = 0, high = 0;
+        impulse = ConvertImpulse(Name, impulse);
+        high += impulse ? 1 : 0;
+        low += impulse ? 0 : 1;
+        //Destinations.ForEach(x => queue.Enqueue((impulse, x)));
+        foreach (var m in Destinations)
+        {
+            Console.WriteLine(Name + " -> " + impulse + " -> " + m.Name);
+
+            queue.Enqueue((impulse, m));
+
+        }
+
+        while (queue.TryDequeue(out var current))
+        {
+            (int mlow, int mhigh) = current.module.HandleImpulse(current.impulse);
+            low += mlow;
+            high += mhigh; 
+        }
+
+        return (low, high);
+    }
+
+    public abstract bool ConvertImpulse(string module, bool impulse);
+
+    public static Module GetModule(string name, Dictionary<string, string[]> destinations)
+    {
+        if (name.StartsWith("%"))
+            return new FlipFlopModule(name, destinations);
+        else if (name.StartsWith("&"))
+            return new ConjunctionModule(name, destinations);
+        else if (name == "broadcaster")
+            return new BroadcasterModule(name, destinations);
+        else
+            return new DummyModule(name, destinations);
+        //throw new NotImplementedException();
+    }
+}
+
+
+class FlipFlopModule(string name, Dictionary<string, string[]> destinations) : Module(name, destinations)
+{
+    public bool On { get; set; }
+
+    public override bool ConvertImpulse(string module, bool impulse)
+    {
+        if (!impulse)
+        {
+            On = !On;
+            return On;
+        }
+
+        return impulse;
+    }
+}
+
+class ConjunctionModule(string name, Dictionary<string, string[]> destinations) : Module(name, destinations)
+{
+    ConcurrentDictionary<string, bool> Memory = new();
+
+    public override bool ConvertImpulse(string module, bool impulse)
+    {
+        Memory.AddOrUpdate(module, false, (k,v) => impulse);
+        return !Memory.All(t => t.Value);
+    }
+}
+
+class BroadcasterModule(string name, Dictionary<string, string[]> destinations) : Module(name, destinations)
+{
+    public override bool ConvertImpulse(string module, bool impulse)
+    {
+        return impulse;
+    }
+}
+
+class DummyModule(string name, Dictionary<string, string[]> destinations) : Module(name, destinations)
+{
+    public new void HandleImpulse(bool impulse)
+    {
+    }
+
+    public override bool ConvertImpulse(string module, bool impulse)
+    {
+        return impulse;
+    }
+}

da5c6735f059f8707f8bc182285cb416451be610
Michael Morf <michael.morf@codemand.at>
2023-12-23 01:14:47 +0100

    level20 part2 by hyper neutrino

--- level20/Program.cs
+++ level20/Program.cs
@@ -1,6 +1,4 @@
-﻿
-using System.Collections.Concurrent;
-using System.ComponentModel;
+﻿using System.Numerics;
 
 string[] lines = File.ReadAllLines("input.txt");
 
@@ -33,8 +31,15 @@ foreach (var c in conjunctions)
 long high = 0;
 long low = 0;
 
-for (int i = 0; i < 1000; i++)
+var before_rx = destinations.Single(t => t.Value.Contains("rx")).Key;
+
+Dictionary<string,int> seen = destinations.Where(t => t.Value.Contains(before_rx)).ToDictionary(x => x.Key, x => 0);
+Dictionary<string, int> cycles = new();
+
+int i = 0;
+while (true)
 {
+    i++;
     queue.Enqueue(("button", ["broadcaster"], null, false));
     while (queue.TryDequeue(out var q))
     {
@@ -49,7 +54,21 @@ for (int i = 0; i < 1000; i++)
 
         foreach (var d in q.destinations)
         {
+            if (d == before_rx && impulse.Value)
+            {
+                seen[q.source]++;
 
+                if (!cycles.ContainsKey(q.source))
+                    cycles.Add(q.source, i);
+                else
+                    if (i != seen[q.source] * cycles[q.source]) throw new Exception("assertion");
+
+                if (seen.All(x => x.Value > 100))
+                {
+                    Console.WriteLine(cycles.Aggregate(1l, (a, b) => lcm(a, b.Value)));
+                    return;
+                }
+            }
             low += impulse.Value ? 0 : 1;
             high += impulse.Value ? 1 : 0;
             //Console.WriteLine(q.source + " -" + (impulse.Value ? "high" : "low") + "-> " + d);
@@ -62,6 +81,24 @@ for (int i = 0; i < 1000; i++)
 Console.WriteLine(low);
 Console.WriteLine(high);
 Console.WriteLine(low * high);
+
+
+
+static long gcf(long a, long b)
+{
+    while (b != 0)
+    {
+        long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+static long lcm(long a, long b)
+{
+    return (a / gcf(a, b)) * b;
+}
 
 class Module(string name)
 {
